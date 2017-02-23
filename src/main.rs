d12f71388f1ff440440173360ad7de64a107236b
Gabriel Smith <ga29smith@gmail.com>
2017-02-23 17:10:38 -0500

    Rework to main function to use clap
    Also makes it more readable

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 #[macro_use]
 extern crate nom;
+#[macro_use]
+extern crate clap;
 
 mod inner_tree;
 mod cpp_parser;
 mod dts_parser;
 mod change_tracker;
 
-use std::env;
 use std::process::Command;
 use std::path::{ Path, PathBuf };
 use std::fs::remove_file;
@@ -25,7 +26,7 @@ const CPP_OUTPUT_NAME: &'static str = "dts_viewer_tmp.dts";
  *  Find byte starts/ends for each file
  *  Parse file to create device tree
  *  create mapping of objects to byte offset starting points
-    - not perfect with offsets stored in object, but it will do
+ *  - not perfect with offsets stored in object, but it will do
  *  create mapping of full paths to objects
  *  create mapping of labels to full paths
  *  Parse device tree to create changes
@@ -36,16 +37,19 @@ const CPP_OUTPUT_NAME: &'static str = "dts_viewer_tmp.dts";
  *  Oh, and somehow display the damn information
  */
 fn main() {
-    let file_name = match env::args().nth(1) {
-        None => {
-            println!("You forgot the dts file, you dummy");
-            return;
-        }
-        Some(x) => x,
-    };
+    let matches = clap_app!(dts_viewer =>
+            (version: crate_version!())
+            (author: "Gabriel s. <ga29smith@gmail.com>")
+            (@arg file: +required "DTS file to parse")
+            (@arg no_defaults: -n --no_defaults "Disable default includes. \
+                An 'include' directory, if it exists, is automatically included")
+            (@arg include: -I ... +takes_value "Additional files to pass to CPP as an include")
+        ).get_matches();
 
-    let file = Path::new(&file_name);
-    let parent = file.parent().unwrap();
+    let file_name = matches.value_of("file").unwrap();
+
+    let file = Path::new(file_name);
+    let parent = file.parent().expect("Could not get parent directory of file");
 
     let mut cpp_command = Command::new("arm-linux-gnueabi-gcc");
     cpp_command.args(&["-H", "-E", "-nostdinc"])
@@ -53,20 +57,30 @@ fn main() {
         .args(&["-o", CPP_OUTPUT_NAME])
         .arg(&file_name);
 
-    // TODO: cmd line option to turn off automatic includes just grab the file
-    // TODO: allow passing of additional include directories
-    // if default_includes {
-    cpp_command.args(&["-I", parent.to_str().unwrap()]);
-    let include_dir = parent.join("include/");
-    if include_dir.is_dir() {
-        cpp_command.args(&["-I", parent.join("include/").to_str().unwrap()]);
+    if !matches.is_present("no_defaults") {
+        cpp_command.args(&["-I", parent.to_str().unwrap()]);
+        let include_dir = parent.join("include/");
+        if include_dir.is_dir() {
+            cpp_command.args(&["-I", include_dir.to_str().unwrap()]);
+        }
     }
-    // }
 
-    let include_output = cpp_command.output().expect("failed to execute process");
+    if let Some(includes) = matches.values_of("include") {
+        for include in includes {
+            let include_dir = parent.join(include);
+            if include_dir.is_dir() {
+                cpp_command.args(&["-I", include_dir.to_str().unwrap()]);
+            }
+        }
+    }
 
+    let include_output = cpp_command.output().expect("Failed to execute CPP");
     let cpp_stderr = String::from_utf8_lossy(&include_output.stderr);
-    println!("{}", cpp_stderr);
+    if !include_output.status.success() {
+        println!("Failed to execute CPP. Error message is below.");
+        print!("{}", cpp_stderr);
+        return;
+    }
 
     let (include_tree, buffer) =
         match parse_cpp_outputs(&cpp_stderr, Path::new(CPP_OUTPUT_NAME), &file_name) {
@@ -77,78 +91,17 @@ fn main() {
             }
         };
 
-    println!("{}", include_tree);
-    //println!("{}", String::from_utf8_lossy(&buffer));
-
-    match parse_dt(&buffer) {
-        Ok(dt) => {
-            //println!("{:#?}", dt);
-            let (boot_info, ammends) = dt;
-            let mut store = LabelStore::new();
-            store.fill(&boot_info, &ammends);
-
-            loop {
-                print!("Enter alias or path: ");
-                io::stdout().flush().expect("Error flushing stdout");
-
-                let mut line = String::new();
-                let stdin = io::stdin();
-                stdin.lock().read_line(&mut line).expect("Error reading from stdin");
-                let line = line.trim();
-
-                if line.is_empty() {
-                    break;
-                }
-
-                let path = if line.starts_with('/') {
-                    Some(PathBuf::from(line))
-                } else {
-                    match store.path_from_label(line) {
-                        Some(path) => {
-                            println!("Path: {:?}", path);
-                            Some(path.to_path_buf())
-                        }
-                        None => {
-                            println!("Label points to no path");
-                            None
-                        }
-                    }
-                };
-
-                if let Some(path) = path {
-                    match store.changes_from_path(&path) {
-                        Some(changes) => {
-                            for change in changes {
-                                let offset = change.get_offset();
-                                println!("Start offset: {}", offset);
-
-                                match include_tree.file_from_offset(offset) {
-                                    Ok(file) => {
-                                        print!("Include file: {:?}", file.path);
-
-                                        match include_tree.file_line_from_global(&buffer, offset) {
-                                            Ok((line, col)) => 
-                                                println!(", Line: {}, Column: {}", line, col),
-                                            Err(err) => println!("{}", err),
-                                        }
-                                    }
-                                    Err(err) => println!("{}", err),
-                                }
-                                println!("{}\n", change);
-                            }
-                        }
-                        None => println!("Nothing at path"),
-                    }
-                }
-            }
-
-            // TODO:
-            // if label covers multiple paths (after saving old aliases is implemented)
-            //     ask user which one they want as well as why this is being asked
-            // show all changes along the way
-        },
-        Err(err) => println!("{:?}", err),
+    if let Err(e) = remove_file(Path::new(CPP_OUTPUT_NAME)) {
+        println!("Failed to delete temp file: {:?}", e);
     }
+
+    println!("{}", include_tree);
+    // println!("{}", String::from_utf8_lossy(&buffer));
+
+    let (boot_info, ammends) = match parse_dt(&buffer) {
+        Ok(dt) => dt,
+        Err(err) => { println!("{:?}", err); return }
+    };
 
     // TODO: perform secondary checks and jazz (only smooth)
     /*
@@ -179,8 +132,67 @@ fn main() {
             fixup path refs
      */
 
-    match remove_file(Path::new(CPP_OUTPUT_NAME)) {
-        Ok(_) => {},
-        Err(e) => println!("Failed to delete temp file: {:?}", e),
+    let mut store = LabelStore::new();
+    store.fill(&boot_info, &ammends);
+
+    loop {
+        print!("Enter alias or path: ");
+        io::stdout().flush().expect("Error flushing stdout");
+
+        let mut line = String::new();
+        let stdin = io::stdin();
+        stdin.lock().read_line(&mut line).expect("Error reading from stdin");
+        let line = line.trim();
+
+        if line.is_empty() {
+            break;
+        }
+
+        let path = if line.starts_with('/') {
+            Some(PathBuf::from(line))
+        } else {
+            let line = if line.starts_with('&') {
+                &line[1..]
+            } else {
+                line
+            };
+            // TODO:
+            // if label covers multiple paths (after saving old aliases is implemented)
+            //     ask user which one they want as well as why this is being asked
+            // show all changes along the way
+            match store.path_from_label(line) {
+                Some(path) => {
+                    println!("Path: {}", path.to_string_lossy());
+                    Some(path.to_path_buf())
+                }
+                None => {
+                    println!("Label points to no path");
+                    None
+                }
+            }
+        };
+
+        if let Some(path) = path {
+            match store.changes_from_path(&path) {
+                Some(changes) => {
+                    for change in changes {
+                        let offset = change.get_offset();
+                        // println!("Start offset: {}", offset);
+                        match include_tree.file_from_offset(offset) {
+                            Ok(file) => {
+                                print!("File: {}", file.path.to_string_lossy());
+                                match include_tree.file_line_from_global(&buffer, offset) {
+                                    Ok((line, col)) => println!(", Line: {}, Column: {}", line, col),
+                                    Err(err) => println!("{}", err),
+                                }
+                            }
+                            Err(err) => println!("{}", err),
+                        }
+                        println!("{}\n", change);
+                    }
+                }
+                None => println!("Nothing at path"),
+            }
+        }
     }
 }
