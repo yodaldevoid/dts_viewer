aca707d9ec76c4c02e39c2c8690145b898af027c
Gabriel Smith <ga29smith@gmail.com>
2017-05-08 14:28:07 -0400

    Update iter_dts_files to use rustc-test so leverage the test framework.
    Tests are output compressed by default, but can be printed normally by running
    "cargo test -- --verbose".

--- device_tree_source/tests/iter_dts_files.rs
+++ device_tree_source/tests/iter_dts_files.rs
@@ -1,50 +1,53 @@
-extern crate device_tree_source;
-extern crate rayon;
-extern crate mktemp;
+#![allow(unused_variables)]
 
-use std::fs::DirEntry;
+extern crate device_tree_source;
+extern crate mktemp;
+extern crate test;
+
+use std::env::args;
 use std::process::Command;
-use std::path::{PathBuf};
+use std::path::{PathBuf, Path};
 
 use mktemp::Temp;
-use rayon::prelude::*;
+
+use test::{test_main, TestDesc, TestDescAndFn, DynTestName, DynTestFn, ShouldPanic};
 
 use device_tree_source::parser::{ParseResult, parse_dt};
 use device_tree_source::include::{IncludeError, include_files};
 
-#[test]
-#[ignore]
-fn iter_test_dts_files() {
-    let path = PathBuf::from("tests/dts");
-    let entries: Vec<_> = path.read_dir().expect("tests/dts directory does not exist")
-                      .filter_map(|e| e.ok())
-                      .filter(|e| e.file_name().to_string_lossy().ends_with(".dts"))
-                      .collect();
-    let mut oks = Vec::new();
-    let mut errs = Vec::new();
-    let results: Vec<_> = entries.par_iter().map(try_parse).collect();
-    for r in results {
-        match r {
-            Ok(p) => oks.push(p),
-            Err(p) => errs.push(p),
-        }
-    }
+fn main() {
+    let args: Vec<_> = args().collect();
 
-    assert_eq!(errs.len(), 0)
+    let tests = Path::new("tests/dts")
+                    .read_dir().expect("tests/dts directory does not exist")
+                    .filter_map(|e| e.ok())
+                    .filter(|e| e.file_name().to_string_lossy().ends_with(".dts"))
+                    .map(|d| d.path())
+                    .map(|p| {
+                        TestDescAndFn {
+                            desc: TestDesc {
+                                name: DynTestName(format!("{}", p.display())),
+                                ignore: true,
+                                should_panic: ShouldPanic::No,
+                            },
+                            testfn: DynTestFn(Box::new(move || try_parse(p.clone()))),
+                        }
+                    })
+                    .collect();
+    test_main(&args, tests);
 }
 
-fn try_parse(e: &DirEntry) -> Result<PathBuf, PathBuf> {
-    let file_name = e.path();
-    let cpp_temp_out = Temp::new_file().expect("Could not create temp file");
+fn try_parse(file_name: PathBuf) {
+    let cpp_temp_out = Temp::new_file().expect("\nCould not create temp file");
     let include_output = {
-        let parent = file_name.parent().expect("Could not get parent directory of file");
+        let parent = file_name.parent().expect("\nCould not get parent directory of file");
 
         let mut cpp_command = Command::new("gcc");
         cpp_command.args(&["-E", "-nostdinc"])
-            .args(&["-undef", "-D__DTS__", "-x", "assembler-with-cpp"])
-            .args(&["-o", cpp_temp_out.as_ref().to_str().unwrap()])
-            .arg(file_name.to_str().unwrap())
-            .args(&["-I", parent.to_str().unwrap()]);
+                   .args(&["-undef", "-D__DTS__", "-x", "assembler-with-cpp"])
+                   .args(&["-o", cpp_temp_out.as_ref().to_str().unwrap()])
+                   .arg(file_name.to_str().unwrap())
+                   .args(&["-I", parent.to_str().unwrap()]);
 
         let include_dir = parent.join("include/");
         if include_dir.is_dir() {
@@ -53,55 +56,49 @@ fn try_parse(e: &DirEntry) -> Result<PathBuf, PathBuf> {
 
         // println!("{:?}", cpp_command);
 
-        cpp_command.output().expect("Failed to start CPP")
+        cpp_command.output().expect("\nFailed to start CPP")
     };
 
     if !include_output.status.success() {
-        println!("Failed to execute CPP. Error message is below.");
-        print!("{}", String::from_utf8_lossy(&include_output.stderr));
-        return Err(file_name);
+        panic!("\nFailed to execute CPP. Error message is below.\n{}",
+               String::from_utf8_lossy(&include_output.stderr));
     }
 
     let (buffer, bounds) = match include_files(&cpp_temp_out) {
         Ok(x) => x,
         Err(e) => {
-            println!("{}:", file_name.display());
             match e {
                 IncludeError::IOError(err, path) => {
-                    print!("IO error: {}", err);
                     if let Some(path) = path {
-                        println!(" {}", path.display());
+                        panic!("\nIO error: {} {}", err, path.display());
+                    } else {
+                        panic!("\nIO error: {}", err);
                     }
                 }
                 IncludeError::LinemarkerInDtsi(path) =>
-                    println!("Extraneous linemarker found in DT include: {}",
-                             path.to_string_lossy()),
+                    panic!("\nExtraneous linemarker found in DT include: {}",
+                           path.to_string_lossy()),
                 IncludeError::ParseError(_) =>
-                    println!("Failed to convert line to byte offset for bounds tracking."),
+                    panic!("\nFailed to convert line to byte offset for bounds tracking."),
                 IncludeError::NoBoundReturned(path) =>
-                    println!("No bounds returned after parsing file: {}", path.to_string_lossy()),
+                    panic!("\nNo bounds returned after parsing file: {}", path.to_string_lossy()),
             }
-            return Err(file_name);
         }
     };
 
     // println!("{:#?}", bounds);
 
     match parse_dt(&buffer) {
-        Ok(ParseResult::Complete(..)) => Ok(file_name),
+        Ok(ParseResult::Complete(..)) => {},
         Ok(ParseResult::RemainingInput(boot_info, amends, rem)) => {
-            println!("{}:", file_name.display());
             // println!("Boot Info:\n{:#?}", boot_info);
             // println!("Amends:\n{:#?}", amends);
-            // println!("Input remaining after parsing:\n\"{}\"", String::from_utf8_lossy(rem));
-            println!("Input remaining after parsing");
-            Err(file_name)
+            panic!("\nInput remaining after parsing:\n{}", String::from_utf8_lossy(rem));
         }
         Err(err) => {
-            println!("{}: {:?}", file_name.display(), err);
             // println!("{:#?}", bounds);
             // println!("{}", String::from_utf8_lossy(&buffer));
-            Err(file_name)
+            panic!("\n{:?}", err);
         }
     }
 }
